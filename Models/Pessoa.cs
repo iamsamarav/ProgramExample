bfe0528a2dd7a54f64ad7b5e67d1437b32abb739
Samara Vitória <devsamaravitoria@gmail.com>
2024-01-01 20:55:17 -0300

    feat: Novo atributo

--- Models/Pessoa.cs
+++ Models/Pessoa.cs
@@ -7,35 +7,59 @@ namespace ProgramExample.Models
 {
     public class Pessoa
     {
-        public Pessoa(string nome, int idade){
+        //Construtor
+        public Pessoa(string nome, int idade, string cpf)
+        {
 
             Nome = nome;
             Idade = idade;
+            Cpf = cpf;
 
         }
+        //Atributos privados
         private string _nome;
         private int _idade;
-
-        public int Idade { 
+        private string _cpf;
+        //Camada de encapsulamento
+        public int Idade 
+        { 
             get => _idade; 
-            set{
-             if(value > 0){
+            set
+            {
+             if(value > 0)
+             {
                 _idade = value;
              }   
             } 
             }
-        public string Nome { 
+        public string Nome 
+        { 
             get => _nome; 
-            set{
-                if(value != null && value.Length > 4){
+            set
+            {
+                if(value != null && value.Length > 4)
+                {
                     _nome = value;
                 }
             } 
+        }
+
+        public string Cpf {
+            get => _cpf;
+            set{
+                if (value.Length == 11)
+                {
+                    _cpf = value;
+                }
+            }
         }
 
         public void Apresentar(){
             Console.WriteLine($"Aluno: {Nome} e {Idade}");
         }
 
+        public void Testar(){
+            
+        }
     }
 }

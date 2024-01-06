a75c54091c065ff4450ae70512c8eeac687de367
Samara Vitória <devsamaravitoria@gmail.com>
2024-01-05 23:24:11 -0300

    feat: Construção do cliente

--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -2,14 +2,24 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ProgramExample.Models;
 
 namespace ProgramExample.Models
 {
     public class Cliente : Pessoa
     {
-        public Cliente()
+
+        private Gerente Gerente { get; set; }
+
+        public Cliente(Gerente gerente)
         {
-            
+            Gerente = gerente;
+
+        }
+
+        public void GetGerente()
+        {
+            Gerente.GetGerente(Gerente.GetNome());
         }
     }
 }

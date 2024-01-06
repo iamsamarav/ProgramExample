44443767ac2d0d4cb0e9c19472c5d1410534c11a
Samara Vitória <devsamaravitoria@gmail.com>
2024-01-05 23:23:33 -0300

    feat: Validação dos métodos

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ProgramExample.Models;
+
+Gerente usuarioG = new Gerente("Paulo", 35, "000.000.000-00", 6500);
+
+Cliente usuarioC = new Cliente(usuarioG);
+
+string nome = usuarioG.GetNome();
+
+usuarioC.GetGerente(nome);
+
+//Manter só o GetNome
